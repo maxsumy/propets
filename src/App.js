da6c22a45b57d16dbe2a584f1d6cb7fa410a8df3
maxsumy <grenki2@gmail.com>
2020-04-11 12:54:44 +0300

    add templ.css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import Test from "./components/Test";
-// import Adding_new_lost from "./components/Adding_new_lost";
-// import Adding_new_found from "./components/Adding_new_found";
+import Adding_new_lost from "./components/Adding_new_lost";
+import Adding_new_found from "./components/Adding_new_found";
 import Sign from "./components/Sing";
 import Templ from "./components/Templ";
-// import './App.css';
+import Confirmation_post from "./components/Confirmation_post";
+import './App.css';
+
 
 function App() {
   return (
     <div>
-      {/*<Test/>*/}
-      <Templ/>
+     <Confirmation_post/>
     </div>
   );
 }
